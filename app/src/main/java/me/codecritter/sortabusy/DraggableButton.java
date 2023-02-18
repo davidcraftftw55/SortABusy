867130f86074771f9a3c2fc9fbba22b52df6bb4f
Codecritter <davidalang55@gmail.com>
2023-02-17 21:04:03 -0800

    Committing progress to transfer computers

--- app/src/main/java/me/codecritter/sortabusy/DraggableButton.java
+++ app/src/main/java/me/codecritter/sortabusy/DraggableButton.java
@@ -3,28 +3,55 @@ package me.codecritter.sortabusy;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 
+/**
+ * A custom implementation of a Button which allows the user to drag on it to either resize or move it
+ */
 public class DraggableButton extends AppCompatButton {
+
+    private static final int MIN_HEIGHT = 200;
 
     private enum DRAG_TYPE { MOVE, RESIZE_TOP, RESIZE_BOT }
 
     private boolean isClick;
-    private float dX;
-    private float dY;
     private DRAG_TYPE dragType;
+    /**
+     * Difference between this button's absolute y coordinate and its getY()
+     */
+    private float dY;
+    /**
+     * Difference from top of button to tap location
+     */
+    private float tapDy;
 
+    /**
+     * Constructs this button (typically only used by code)
+     * @param context context needed
+     */
     public DraggableButton(@NonNull Context context) {
         super(context);
     }
 
+    /**
+     * Constructs this button (typically used by LayoutInflaters)
+     * @param context context needed
+     * @param attrs set of attributes assigned by the layout file
+     */
     public DraggableButton(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
+    /**
+     * Constructs this button (typically used by LayoutInflaters), with a specified default style
+     * @param context context needed
+     * @param attrs set of attributes assigned by the layout file
+     * @param defStyleAttr resource id for a default style (0 to keep current default style)
+     */
     public DraggableButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
@@ -34,13 +61,16 @@ public class DraggableButton extends AppCompatButton {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 isClick = true;
-                dX = getX() - event.getRawX();
-                dY = getY() - event.getRawY();
 
-                float y = event.getRawY() + dY;
-                if (y < getY() + getHeight() * 0.25) {
+                int[] coords = new int[2];
+                getLocationOnScreen(coords);
+                float tapDx = event.getRawX() - coords[0];
+                dY = coords[1] - getY();
+                tapDy = event.getRawY() - coords[1];
+
+                if (tapDx < getWidth() * 0.33) {
                     dragType = DRAG_TYPE.RESIZE_TOP;
-                } else if (y < getY() + getHeight() * 0.75) {
+                } else if (tapDx < getWidth() * 0.67) {
                     dragType = DRAG_TYPE.MOVE;
                 } else {
                     dragType = DRAG_TYPE.RESIZE_BOT;
@@ -49,8 +79,38 @@ public class DraggableButton extends AppCompatButton {
                 break;
             case MotionEvent.ACTION_MOVE:
                 isClick = false;
-                setX(event.getRawX());
-                setY(event.getRawY());
+
+                switch(dragType) {
+                    case RESIZE_TOP:
+                        float newTop = event.getRawY() - dY - tapDy;
+                        int heightIncrease = Math.round(getY() - newTop);
+
+                        if (getHeight() + heightIncrease > MIN_HEIGHT) {
+                            setY(newTop);
+
+                            ViewGroup.LayoutParams params = getLayoutParams();
+                            params.height += heightIncrease;
+                            setLayoutParams(params);
+                        }
+                        break;
+                    case MOVE:
+                        setY(event.getRawY() - dY - tapDy);
+                        break;
+                    case RESIZE_BOT:
+                        heightIncrease = Math.round(event.getRawY() - dY - tapDy - getY());
+                        // FIXME stretches perpetually
+                        //  (it seems that the height increases/decreases by distance from original touch)
+                        //  (like it uses velocity instead of positioning
+
+
+                        if (getHeight() + heightIncrease > 200) {
+                            ViewGroup.LayoutParams params = getLayoutParams();
+                            params.height += heightIncrease;
+                            setLayoutParams(params);
+                        }
+                        break;
+                }
+
                 break;
             case MotionEvent.ACTION_UP:
                 if (isClick) {
--- app/src/main/java/me/codecritter/sortabusy/MainActivity.java
+++ app/src/main/java/me/codecritter/sortabusy/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * The Main Activity for this app is the Schedule Maker, where user will be able to edit their
+ * schedule on a given day; they should also be able to navigate to the other pages from this activity
+ */
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setX(350);
     }
 }
